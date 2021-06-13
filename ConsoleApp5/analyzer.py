e22f8f0a6df953a6c85344c8284842cc7072b7bc
adons1 <32033752+adons1@users.noreply.github.com>
2021-06-13 23:13:12 +0300

    Add files via upload

--- ConsoleApp5/analyzer.py
+++ ConsoleApp5/analyzer.py
@@ -1,28 +1,30 @@
 from PIL import Image
 import pytesseract
-import speech_recognition as sr
 import os
 import re
 
 def ImageRecognizer(file):
     try:
         img = Image.open(file)
-        text = pytesseract.image_to_string(img, lang='rus')
+        text = "Русский\n"+pytesseract.image_to_string(img, lang='rus') + "\nАнглийский\n" +pytesseract.image_to_string(img, lang='eng')
         os.remove(file)
         return text
     except:
         print("error:"+ file)
-    
-mypath = "userphotos/"
-files = os.listdir(mypath) 
-result = str()
-for file in files:
-    result=result+ImageRecognizer(mypath+file)
 
-result=re.sub(' +', ' ', result)
-result=re.sub(' \r', ' ', result)
-result=re.sub(' \n', ' ', result)
+def AnalyzePhotos():
+    mypath = "userphotos/"
+    files = os.listdir(mypath) 
+    result = str()
+    for file in files:
+        result=result+ImageRecognizer(mypath+file)
 
-output_file = open("alalisys.txt", "w")
-output_file.write(result)
-output_file.close()
+    result=re.sub(' +', ' ', result)
+    result=re.sub(' \r', ' ', result)
+    result=re.sub(' \n', ' ', result)
+
+    output_file = open("analisys_photos.txt", "w", encoding='utf-8')
+    output_file.write(result)
+    output_file.close()
+
+AnalyzePhotos()
